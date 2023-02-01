119279c420e6553a09dc9b04533359325572e354
Vortex077 <avisri9815@outlook.com>
2023-02-01 16:48:00 +0530

    cmt_msg_022

--- first.py
+++ first.py
@@ -1,4 +1,12 @@
-#------Pyhton file..........
-
-
-#------------
+class shape:
+    pass
+       
+class circle(shape):
+    def m(self):
+        print("The area of circle is: 100")
+        print("The color of circle is: Blue")
+ 
+class rectangle(shape):
+    def m(self):
+        print("The area of rectangle is: 100")
+        print("The color of rectangle is: Red")
